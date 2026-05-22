0c15e4c3594ded834b84ee31042cf5cc836d3949
hahafoot <gordondoan13@gmail.com>
2026-05-22 00:27:11 -0400

    Add ASCII art logo and update Fastfetch configuration for enhanced display

--- dotfiles/hypr/workspace-watcher.sh
+++ dotfiles/hypr/workspace-watcher.sh
@@ -14,7 +14,7 @@ set -u
 declare -A RULES=(
     # ---- Desktop 1: terminal, file explorer, programming ----
     # ['^kitty$']=1
-    ['[Nn]autilus']=1
+    # ['[Nn]autilus']=1
     ['^[Cc]ode$']=1
     ['[Kk]ate']=1
     ['[Tt]ext[Ee]ditor']=1
